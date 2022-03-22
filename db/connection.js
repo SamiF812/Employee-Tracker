4d6b731b25544d2eb6868253025074ce4d9108a3
Sami Fares <djsamif@gmail.com>
2022-03-22 18:40:29 -0400

    changes to index.js

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const util = require("util");
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   // Your username
   user: "root",
   // Your password
--- db/index.js
+++ db/index.js
@@ -8,18 +8,18 @@ class DB {
 
   // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() {
-    return this.connection.promise().query(
+    return this.connection.query(
       // CREATE SELECT STATMENT WITH THE FOLLOWING COLUMNS FROM THREE TABLES.
       // id, first_name, last_name FROM employee TABLE AND department name from department TABLE AND SELECT salary FROM role TABLE
       // YOUR NEED TO USE LEFT JOINS TO JOIN THREE TABLES
       // TODO: YOUR CODE HERE
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN deparment on role.department_id = department.id"
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id"
     );
   }
 
   // Find all employees except the given employee id
   findAllPossibleManagers(employeeId) {
-    return this.connection.promise().query(
+    return this.connection.query(
       "SELECT id, first_name, last_name FROM employee WHERE id != ?",
       employeeId
     );
@@ -27,13 +27,13 @@ class DB {
 
   // Create a new employee
   createEmployee(employee) {
-    return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    return this.connection.query("INSERT INTO employee SET ?", employee);
   }
 
 
   // Update the given employee's role
   updateEmployeeRole(employeeId, roleId) {
-    return this.connection.promise().query(
+    return this.connection.query(
       // TODO: YOUR CODE HERE
       "UPDATE employee SET role_id = ? WHERE id = ?",
       [roleId, employeeId]
@@ -43,7 +43,7 @@ class DB {
 
   // Update the given employee's manager
   updateEmployeeManager(employeeId, managerId) {
-    return this.connection.promise().query(
+    return this.connection.query(
       "UPDATE employee SET manager_id = ? WHERE id = ?",
       [managerId, employeeId]
     );
@@ -51,7 +51,7 @@ class DB {
 
   // Find all roles, join with departments to display the department name
   findAllRoles() {
-    return this.connection.promise().query(
+    return this.connection.query(
       // SELECT THE FOLLOWING COLUMNS:
       // id, title, salary FROM role TABLE AND department name FROM department TABLE
       // YOU NEED TO USE LEFT JOIN TO JOIN role and department TABLES
@@ -63,7 +63,7 @@ class DB {
 
   // Create a new role
   createRole(role) {
-    return this.connection.promise().query(
+    return this.connection.query(
       // TODO: YOUR CODE HERE
       "INSERT INTO role SET ?", role
 
@@ -73,17 +73,14 @@ class DB {
 
   // Find all departments, join with employees and roles and sum up utilized department budget
   findAllDepartments() {
-    return this.connection.promise().query(
-      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget \
-      FROM department LEFT JOIN role ON role.department_id = department.id \
-      LEFT JOIN employee ON employee.role_id = role.id \
-      GROUP BY department.id, department.name"
+    return this.connection.query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
     );
   }
 
   // Create a new department
   createDepartment(department) {
-    return this.connection.promise().query(
+    return this.connection.query(
       // TODO: YOUR CODE HERE
       "INSERT INTO department SET ?", department
     );
@@ -91,7 +88,7 @@ class DB {
 
   // Find all employees in a given department, join with roles to display role titles
   findAllEmployeesByDepartment(departmentId) {
-    return this.connection.promise().query(
+    return this.connection.query(
       "SELECT employee.id, employee.first_name, employee.last_name, role.title \
       FROM employee \
       LEFT JOIN role on employee.role_id = role.id \
